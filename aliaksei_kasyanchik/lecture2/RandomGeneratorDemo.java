a6cb34a67ef8962b14ab0479318cfd4a507d5a17
kasyanl <74323441+kasyanl@users.noreply.github.com>
2020-11-13 11:23:07 +0300

    Update RandomGeneratorDemo.java

--- aliaksei_kasyanchik/lecture2/RandomGeneratorDemo.java
+++ aliaksei_kasyanchik/lecture2/RandomGeneratorDemo.java
@@ -6,11 +6,11 @@ public class RandomGeneratorDemo {
 		int randomNumber2 =(int) (Math.random()*100);
 		int randomNumber3 =(int) (Math.random()*100);
        
-                System.out.println ("Случайное число №1 = " + randomNumber1);
-                System.out.println ("Случайное число №2 = " + randomNumber2);
-                System.out.println ("Случайное число №3 = " + randomNumber3);
-                System.out.println ();
-                System.out.println ("Сумма этих чисел равна: " +(randomNumber1 + randomNumber2 + randomNumber3));
+               System.out.println ("Random number 1 = " + randomNumber1);
+               System.out.println ("Random number 2 = " + randomNumber2);
+               System.out.println ("Random number 3 = " + randomNumber3);
+               System.out.println ();
+               System.out.println ("Addition random number: " +(randomNumber1 + randomNumber2 + randomNumber3));
 	}
 
 }
